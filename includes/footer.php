ad3ae1999a1963a71a1d0a7af8e363adc2c9cd6e
faiza00145 <86762704+faiza00145@users.noreply.github.com>
2021-07-01 12:00:19 -0800

    My Ecommerce Website

---
+++ includes/footer.php
@@ -0,0 +1,160 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Follow Links</h4>
+                
+                <ul><!-- ul Begin -->
+                   <li><a href="index.php">Home</a></li>
+                   <li><a href="shop.php">Shop</a></li>
+                   <li><a href="customer/my_account.php">My Account</a></li>
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    
+                    
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>User Links </h4>
+                
+                <ul><!-- ul Begin -->
+                           
+                           <?php 
+                           
+                           if(!isset($_SESSION['customer_email'])){
+                               
+                               echo"<a href='checkout.php'>Login</a>";
+                               
+                           }else{
+                               
+                              echo"<a href='customer/my_account.php?my_orders'>My Account</a>"; 
+                               
+                           }
+                           
+                           ?>
+                    
+                    <li><a href="customer_register.php">Sign Up</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Products Categories</h4>
+                
+                <ul><!-- ul Begin -->
+                
+                    <?php 
+                    
+                        $get_p_cats = "select * from product_categories";
+                    
+                        $run_p_cats = mysqli_query($con,$get_p_cats);
+                    
+                        while($row_p_cats=mysqli_fetch_array($run_p_cats)){
+                            
+                            $p_cat_id = $row_p_cats['p_cat_id'];
+                            
+                            $p_cat_title = $row_p_cats['p_cat_title'];
+                            
+                            echo "
+                            
+                                <li>
+                                
+                                    <a href='shop.php?p_cat=$p_cat_id'>
+                                    
+                                        $p_cat_title
+                                    
+                                    </a>
+                                
+                                </li>
+                            
+                            ";
+                            
+                        }
+                    
+                    ?>
+                
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Contact Us</h4>
+                
+                <p><!-- p Start -->
+                    
+                   
+                    <br/>+92300-0000000
+                    <br/>username@gmail.com
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">Our Contact Page</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4> Latest News</h4>
+                
+                <p class="text-muted">
+                    Dont miss our latest  Products
+                </p>
+                
+                <form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=M-devMedia', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                   
+                        <input type="text" class="form-control" name="email">
+                        
+                        <input type="hidden" value="M-devMedia" name="uri"/><input type="hidden" name="loc" value="en_US"/>
+                        
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4> Follow & Touch Us </h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+    </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2021 Tech Software House</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+         
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
